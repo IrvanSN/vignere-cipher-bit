f6d9510a678212cde58dbf03586569f66fe9686d
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-12-05 16:34:56 +0700

    add comment

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-// Perform char plainText to bit
+// fungsi untuk mengkonversi string ke bit
 const charToBit = (char) => {
   const result = [];
 
+  // Loop array char dari parameter
   for (let index = 0; index < char.length; index++) {
+    // convert character ke bit menggunakan method .charCodeAt
+    // menggunakan .padStart() untuk menambahkan padding bit
     const bit = char.charCodeAt(index).toString(2).padStart(8, "0");
+    // push hasil bit ke variable result
     result.push(bit);
   }
 
+  // Return value result
   return result;
 };
 
-// Repeat secretKeyBit elements to match the length of plainTextBit
+// fungsi untuk menyesuaikan array secretKey sesuai panjang array plainText
 const generateSecretKeyBit = (plainTextBitLength, secretKeyBit) => {
   const repeatedSecretKeyBit = [];
   let currentIndex = 0;
@@ -23,65 +28,73 @@ const generateSecretKeyBit = (plainTextBitLength, secretKeyBit) => {
   return repeatedSecretKeyBit;
 };
 
-// Perform XOR bit
+// Fungsi untuk melakukan operasi XOR dari setiap char bit
 const performXOR = (bit1, bit2) => {
+  // menjadikan string ke array
   const bit1Split = bit1.split("");
   const bit2Split = bit2.split("");
 
+  // loop array kemudian setiap bit di XOR kan antara 2 array tsb
   return bit1Split
     .map((_, index) => parseInt(bit1Split[index]) ^ parseInt(bit2Split[index]))
     .join("");
 };
 
-// encode function
+// Fungsi untuk encode plainText menerima parameter plainText dan secretKey
 const encode = (plainText, secretKey) => {
+  // Konversi plainText ke bit menggunakan fungsi charToBit
   const plainTextBit = charToBit(plainText);
+  // Generate secretKeyBit dengan memanggil fungsi generateSecretKeyBit
   const secretKeyBit = generateSecretKeyBit(
     plainTextBit.length,
     charToBit(secretKey)
   );
 
-  console.log("encode plainTextBit", plainTextBit);
-  console.log("encode secretKeyBit", secretKeyBit);
-
+  // Loop variable plainTextBit kemudian panggil fungsi performXOR untuk
+  // melakukan operasi XOR
   const cipherTextBit = plainTextBit.map((_, index) =>
     performXOR(plainTextBit[index], secretKeyBit[index])
   );
 
+  // menggabungkan array menjadi string
   return cipherTextBit.join("");
 };
 
-// decode function
+// Fungsi untuk decode bit ke string
 const decode = (cipherText, secretKey) => {
   const cipherTextBit = [];
 
+  // Pisah cipherBit menjadi 8 index kemudian masukkan ke array cipherTextBit
   for (let i = 0; i < cipherText.length; i += 8) {
     cipherTextBit.push(cipherText.substring(i, i + 8));
   }
 
+  // Generate secretKeyBit dengan fungsi generateSecretKeyBit
   const secretKeyBit = generateSecretKeyBit(
     cipherTextBit.length,
     charToBit(secretKey)
   );
 
-  console.log("decode cipherTextBit", cipherTextBit);
-  console.log("decode secretKeyBit", secretKeyBit);
-
+  // Lakukan XOR pada bit
   const plainTextBit = cipherTextBit.map((_, index) =>
     performXOR(cipherTextBit[index], secretKeyBit[index])
   );
 
-  console.log("decode plainTextBit", plainTextBit);
-
+  // Loop bit kemudian konversi dari bit ke string/char
   const result = plainTextBit.map((item) =>
     String.fromCharCode(parseInt(item, 2))
   );
 
+  // menggabungkan array menjadi string
   return result.join("");
 };
 
-const key = "10101 01010";
-const cipherText = encode("haizooz ss", key);
-console.log("cipherText", cipherText);
-const decodeCipherText = decode(cipherText, key);
-console.log("decodeCipherText", decodeCipherText);
+const key = "5";
+const plainText = "hello";
+console.log("plainText:", plainText);
+console.log("key:", key);
+console.log("");
+const cipherTextBit = encode(plainText, key);
+console.log("cipherTextBit:", cipherTextBit);
+const decodeCipherText = decode(cipherTextBit, key);
+console.log("decodeCipherText:", decodeCipherText);
